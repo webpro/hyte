430c3f78bd136dfe918c80ac070e5642822367d2
Lars Kappert <lars@webpro.nl>
2012-05-28 13:51:18 +0200

    This generated file shouldn't be in the repository

--- public/compiled.js
+++
@@ -1,5 +0,0 @@
-window.app = {};
-window.app.templates = {
-	"list": new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<ul>");_.b("\n" + i);if(_.s(_.f("names",c,p,1),c,p,0,16,37,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("	<li>");_.b(_.v(_.f("name",c,p,0)));_.b("</li>");_.b("\n");});c.pop();}_.b("</ul>");_.b("\n");return _.fl();;}),
-	"paragraph": new Hogan.Template(function(c,p,i){var _=this;_.b(i=i||"");_.b("<p>");_.b(_.v(_.f("message",c,p,0)));_.b("</p>");_.b("\n");return _.fl();;})
-};
